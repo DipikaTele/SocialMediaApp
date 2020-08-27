220bcfb68c0ab06ee2375cab4c8a64e8313f704c
DipikaTele <34939765+DipikaTele@users.noreply.github.com>
2020-08-27 10:33:36 +0530

    basic setup is completed

--- Social/views.py
+++ Social/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from Social import models
 
 
 # Create your views here.
-class wall(ListView):
+class wall(LoginRequiredMixin,ListView):
     queryset = models.Post.objects.all()
     context_object_name = 'posts'
     template_name = 'Social/wall.html'
+    login_url = 'auth/login'
